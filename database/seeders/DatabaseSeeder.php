77a5ca9a496a9d6c22880438eabf991bd98d214c
manuelcamaioni <camaioni.manuel@gmail.com>
2023-08-28 16:08:32 +0200

    updated seeding order and Technology seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
+            TechnologySeeder::class,
             TypeSeeder::class,
             ProjectSeeder::class,
         ]);
---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Technology;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $technologies = ['Vue-Vite', 'Javascript', 'Laravel', 'Node.js', 'React', 'Python'];
+        foreach ($technologies as $technology){
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->save();
+        }
+    }
+}
