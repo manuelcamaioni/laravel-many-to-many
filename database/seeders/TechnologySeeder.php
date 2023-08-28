3ccdc84d993e0861f4d035092f8223322a409c13
manuelcamaioni <camaioni.manuel@gmail.com>
2023-08-28 18:10:13 +0200

    refactored path to technology model

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Technology;
+use App\Models\Admin\Technology;
 
 class TechnologySeeder extends Seeder
 {
